3f95e5595f878ec0326d94749040c604ff619408
relay10 <105665837+relay10@users.noreply.github.com>
2025-04-30 21:21:24 +0100

    Update configs.py

--- utils/configs.py
+++ utils/configs.py
@@ -1,1 +1,50 @@
+# ==== Configurations ==== #
+@dataclass
+class ModelConfig:
+    def __init__(self):
+        self.block_size = 32 # context size (min num of tokens in sequence, how long model can see at once)
+        self.vocab_size = 50257 # This is all the unique tokens + 2 for overhead
+        self.max_iters = 100
+        self.n_head = 4
+        self.n_embd = 64
+        self.n_layers = 6
+        self.dropout = 0.1
+        self.bias: bool = True
+        self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+                                          # switch out 'mps' for 'cuda' when transferring to github or stanage for cuda use
 
+# Hyperparam Tune the training configs (not model configs for hp tuning - only sclae up and train full model when you have found the optimal training params)
+@dataclass
+class TrainConfig:
+    epochs: int = 50
+    batch_size: int = 32
+    max_len: int = 64
+    pad: int = 0
+    grad_accum_steps: int = 1
+    max_lr: float = 3e-4
+    min_lr: float = 1e-5
+    warmup_iters: int = 100
+    n_steps: int = 0
+    lr_decay_iters: int = 1000
+    save_every: int = 1
+    save_every_steps:int = 2000
+
+@dataclass
+class ProxyConfig:
+    epochs:int = 2
+    vocab_size:int = ModelConfig().vocab_size    # keep same vocab
+    block_size:int = ModelConfig().block_size    # same context length
+    batch_size:int = 32
+    n_embd:int = 128                        # e.g. 1/4 or 1/8 of main
+    n_head :int = 4                          # just enough heads
+    n_layers:int = 2                          # really small depth
+    dropout:float = 0.1
+    bias: bool = False
+    min_lr: float = 1e-4
+    max_lr: float = 3e-4
+    device  = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+    # Proxy steps
+    T_steps:int = 1_00
+    t0:int = 5_0
+    step:int = 0
