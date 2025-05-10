ccf3997a4fa72238ad1fbc927d998c784ca86e15
relay10 <105665837+relay10@users.noreply.github.com>
2025-05-10 19:03:50 +0100

    Add files via upload

--- utils/configs.py
+++ utils/configs.py
@@ -5,16 +5,15 @@ import torch
 @dataclass
 class ModelConfig:
     def __init__(self):
-        self.block_size = 32 # context size (min num of tokens in sequence, how long model can see at once)
+        self.block_size = 32
         self.vocab_size = 50256
         self.max_iters = 100
-        self.n_head = 4
-        self.n_embd = 64
-        self.n_layers = 6
-        self.dropout = 0.1
+        self.n_head = 6
+        self.n_embd = 32
+        self.n_layers = 4
+        self.dropout = 0.0079737
         self.bias: bool = True
-        self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-                                          # switch out when transferring to github or stanage for cuda use
+        self.device = torch.device("mps" if torch.mps.is_available() else "cpu")
 
 @dataclass
 class TrainConfig:
@@ -22,34 +21,36 @@ class TrainConfig:
     batch_size: int = 32
     max_len: int = 64
     pad: int = 0
-    grad_accum_steps: int = 1
-    max_lr: float = 3e-4
-    min_lr: float = 1e-5
-    warmup_iters: int = 100
+    grad_accum_steps: int = 2
+    max_lr: float = 3.35329e-05
+    min_lr: float = 0.001739
+    warmup_iters: int = 20
     n_steps: int = 0
     lr_decay_iters: int = 1000
     save_checkpoints: bool = False
     save_every: int = 1
-    save_every_steps:int = 2000
+    save_every_steps: int = 500
+
+    alpha_update_steps:int = 500
+    gamma: float = 0.3 # [non zero]
+    total_steps:int = 1
 
 @dataclass
 class ProxyConfig:
-    epochs:int = 5
-    vocab_size:int = ModelConfig().vocab_size    # keep same vocab
-    block_size:int = ModelConfig().block_size    # same context length
-    batch_size:int = 32
-    n_embd:int = 128                        # e.g. 1/4 or 1/8 of main
-    n_head :int = 4                          # just enough heads
-    n_layers:int = 2                          # really small depth
-    dropout:float = 0.1
+    epochs: int = 5
+    vocab_size: int = ModelConfig().vocab_size
+    block_size: int = ModelConfig().block_size
+    batch_size: int = 16
+    n_embd: int = 64 // 4
+    n_head: int = 2
+    n_layers: int = 2
+    dropout: float = 0.15
     bias: bool = False
-    min_lr: float = 1e-4
-    max_lr: float = 3e-4
-    device  = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    min_lr: float = 0.0001388403383951373
+    max_lr: float = 0.0005018765292380106
+    device = torch.device("mps" if torch.mps.is_available() else "cpu")
+    T_steps: int = 250
+    t0: int = 25
+    step: int = 0
+    alpha_scale: float = 0.5
 
-    # Proxy steps
-    T_steps:int = 1_00
-    t0:int = 5_0
-    step:int = 0
-
-    alpha_scale:float = 0.7 # keep [0,1] range
