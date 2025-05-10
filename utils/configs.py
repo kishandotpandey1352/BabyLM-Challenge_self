917662f05649fe96f2a9fce47b1a7a26ec7a9fa7
relay10 <105665837+relay10@users.noreply.github.com>
2025-05-10 19:25:26 +0100

    Update configs.py

--- utils/configs.py
+++ utils/configs.py
@@ -5,26 +5,26 @@ import torch
 @dataclass
 class ModelConfig:
     def __init__(self):
-        self.block_size = 32
+        self.block_size = 1024
         self.vocab_size = 50256
         self.max_iters = 100
-        self.n_head = 6
-        self.n_embd = 32
-        self.n_layers = 4
-        self.dropout = 0.0079737
+        self.n_head = 12
+        self.n_embd = 768
+        self.n_layers = 12
+        self.dropout = 0.1
         self.bias: bool = True
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @dataclass
 class TrainConfig:
-    epochs: int = 5
-    batch_size: int = 32
-    max_len: int = 64
+    epochs: int = 50
+    batch_size: int = 64
+    max_len: int = 1024
     pad: int = 0
     grad_accum_steps: int = 2
-    max_lr: float = 3.35329e-05
-    min_lr: float = 0.001739
-    warmup_iters: int = 20
+    max_lr: float = 0.001739
+    min_lr: float = 3.35329e-05
+    warmup_iters: int = 1000
     n_steps: int = 0
     lr_decay_iters: int = 1000
     save_checkpoints: bool = False
@@ -37,20 +37,20 @@ class TrainConfig:
 
 @dataclass
 class ProxyConfig:
-    epochs: int = 5
+    epochs: int = 15
     vocab_size: int = ModelConfig().vocab_size
     block_size: int = ModelConfig().block_size
     batch_size: int = 16
-    n_embd: int = 64 // 4
-    n_head: int = 2
-    n_layers: int = 2
+    n_embd: int = 768 // 4
+    n_head: int = 4
+    n_layers: int = 4
     dropout: float = 0.15
     bias: bool = False
     min_lr: float = 0.0001388403383951373
     max_lr: float = 0.0005018765292380106
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    T_steps: int = 250
-    t0: int = 25
+    T_steps: int = 5000
+    t0: int = 100
     step: int = 0
     alpha_scale: float = 0.5
 
