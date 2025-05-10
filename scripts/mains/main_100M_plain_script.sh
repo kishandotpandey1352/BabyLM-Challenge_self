ee1a91b68df1bf9a55b7f545f6246e2805e141fb
relay10 <105665837+relay10@users.noreply.github.com>
2025-05-10 19:01:43 +0100

    Add files via upload

---
+++ scripts/mains/main_100M_plain_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --job-name=main100
+#SBATCH --output=logs/main100M.out
+#SBATCH --error=logs/main100M.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=256G
+#SBATCH --time=30:00:00
+#SBATCH --gres=gpu:1          
+#SBATCH --partition=gpu  
+
+source ~/.bashrc 
+conda activate babylm
+module load CUDA/11.7.0
+
+# Step into the project root
+cd /users/acp24mw/BabyLM-Challenge # change user specific for your user
+
+nvidia-smi
+free -h
+nvidia-smi --query-compute-apps=pid,used_memory --format=csv
+
+# export PYTHONPATH to make sure python finds local packages (change for your user) so bash can find it
+export PYTHONPATH="/users/acp24mw/BabyLM-Challenge:$PYTHONPATH"
+
+python main/gpt_model.py \
+    --data_path tokenizers/100M_data_token.pkl \
+    --scoring composite \
+    --schedule_type sigmoid \
+    --curriculum off \
+    --data_size 100M
