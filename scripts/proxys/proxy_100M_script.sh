49290fad45562b164e3179bfcfa9de281c25d188
relay10 <105665837+relay10@users.noreply.github.com>
2025-05-11 13:29:46 +0100

    Update proxy_100M_script.sh

--- scripts/proxys/proxy_100M_script.sh
+++ scripts/proxys/proxy_100M_script.sh
@@ -16,14 +16,14 @@ conda activate babylm
 module load CUDA/11.7.0
 
 # Step into the project root
-cd /users/acp24mw/BabyLM-Challenge # change user specific for your user
+cd /users/#username#/BabyLM-Challenge # change user specific for your user
 
 nvidia-smi
 free -h
 nvidia-smi --query-compute-apps=pid,used_memory --format=csv
 
 # export PYTHONPATH to make sure python finds local packages (change for your user) so bash can find it
-export PYTHONPATH="/users/acp24mw/BabyLM-Challenge:$PYTHONPATH"
+export PYTHONPATH="/users/#username#/BabyLM-Challenge:$PYTHONPATH"
 
 python main/proxy_main.py \
     --data_path tokenizers/100M_data_token.pkl \
