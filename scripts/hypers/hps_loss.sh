0e2bd38c244ee302abe2bf313802cebfc1bc3b5e
relay10 <105665837+relay10@users.noreply.github.com>
2025-05-12 19:41:05 +0100

    Update hps_loss.sh

--- scripts/hypers/hps_loss.sh
+++ scripts/hypers/hps_loss.sh
@@ -15,7 +15,6 @@ conda activate babylm-gpu
 module load CUDA/12.4
 
 # 
-
 # Step into the project root
 cd /users/{username}/BabyLM-Challenge # change user specific for your user
 # export PYTHONPATH to make sure python finds local packages (change for your user) so bash can find it
@@ -24,6 +23,12 @@ export CUDA_VISIBLE_DEVICES=0
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_LAUNCH_BLOCKING=1
 
+
+
+nvidia-smi
+free -h
+nvidia-smi --query-compute-apps=pid,used_memory --format=csv
+nvidia-smi --loop=1000 > logs/gpu_usage.log &
 
 # Run the script from BabyLM-Challenge
 python hps/param_tuning.py \
